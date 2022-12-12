26869c1515692acccf5bc44e5febc1b882a8a873
Visual Student <visualstudent@zschie.edu.pl>
2022-12-12 09:02:37 +0100

    Dodaj pliki projektów.

---
+++ ZadanieZInterfejsu_Desktopowe_12.12.2022/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZadanieZInterfejsu_Desktopowe_12._12._2022
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
